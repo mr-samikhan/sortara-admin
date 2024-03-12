d81ac3936e68571eaf495d6a5de5b1dfb2affa62
sami-chopdawg <sami@chopdawg.com>
2024-03-12 16:22:38 +0500

    feat:removed conflict

--- src/modules/ManageAuth/containers/LoginContainer/LoginContainer.tsx
+++ src/modules/ManageAuth/containers/LoginContainer/LoginContainer.tsx
@@ -1,9 +1,5 @@
 const LoginContainer = () => {
-<<<<<<< HEAD
   return <div>LoginContainer added</div>
-=======
-  return <div>LoginContainer</div>
->>>>>>> becaa3c4053ce9019123be9532c4810ee860cd31
 }
 
 export default LoginContainer
