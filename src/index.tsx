bb961f236f2ee594271d3efcda29ed5b99e8e5e1
sami-chopdawg <sami@chopdawg.com>
2024-03-12 16:32:27 +0500

    feat:removed all conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,5 @@
 import { App } from '@vgl/core'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-=======
 import { ReduxProvider } from '@vgl/providers'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -17,5 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </BrowserRouter>
   </ReduxProvider>
->>>>>>> becaa3c4053ce9019123be9532c4810ee860cd31
 )
