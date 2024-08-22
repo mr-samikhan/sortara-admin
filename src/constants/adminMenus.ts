60e8a8afc22cf68ddf09b240921a72ae9c52cf8d
sami-chopdawg <sami@chopdawg.com>
2024-08-22 22:17:41 +0500

    project-added

--- src/constants/adminMenus.ts
+++ src/constants/adminMenus.ts
@@ -1,26 +1,26 @@
-import { ROUTES } from './constants'
+import { ROUTES } from "./constants";
 
 const ADMIN_MENUS = [
   {
-    title: 'Moderators',
+    title: "Moderators",
     path: ROUTES.MODERATORS,
-    singlePath: '/moderator',
+    singlePath: "/moderator",
   },
   {
-    title: 'Users',
+    title: "Users",
     path: ROUTES.USERS,
-    singlePath: '/user',
+    singlePath: "/user",
   },
   {
     path: ROUTES.ADS,
-    singlePath: '/ad',
-    title: 'Manage Ads',
+    singlePath: "/ad",
+    title: "Manage Ads",
   },
   {
-    title: 'Analytics',
-    singlePath: '/analytics',
+    title: "Analytics",
+    singlePath: "/analytics",
     path: ROUTES.ANALYTICS || ROUTES.ROOT,
   },
-]
+];
 
-export default ADMIN_MENUS
+export default ADMIN_MENUS;
